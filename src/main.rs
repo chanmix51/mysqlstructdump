cd8e510848bafc5ee3360cf2cfea693f8f06ffa7
Grégoire HUBERT <hubert.greg@gmail.com>
2020-11-04 05:33:15 +0100

    first working version

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use sqlx::mysql::MySqlPool;
+use sqlx::prelude::*;
+use futures::executor::block_on;
+use std::fmt::{self, Formatter, Display};
+
+struct DatabaseInspector {
+    pool: MySqlPool,
+}
+
+#[derive(Debug,sqlx::FromRow)]
+struct TableList {
+    table_name: String,
+    table_type: String,
+    table_rows: Option<u32>,
+    index_length: Option<u32>,
+    auto_increment: Option<u8>,
+}
+
+impl Display for TableList {
+   fn fmt(&self, f: &mut Formatter) -> fmt::Result { 
+       let table_type = match self.table_type.as_str() {
+            "BASE TABLE"    => "TABLE",
+            "VIEW"          => "VIEW",
+            _               => "UNKNOWN",
+            };
+
+        let table_rows = match self.table_rows {
+            Some(o) => format!("{:5}", o),
+            None    => format!("{:>5}", "no"),
+        };
+
+        let index_length = match self.index_length {
+            Some(o) => format!("{:12}", o),
+            None    => format!("{:>12}", "no"),
+        };
+
+        let auto_increment = match self.auto_increment {
+            Some(1)     => "Y".to_string(),
+            _           => "N".to_string(),
+        };
+        
+       write!(f, "{:5} {:>15} | {} rows | {} bytes | {}", table_type, self.table_name, table_rows, index_length, auto_increment)
+   }
+}
+
+impl DatabaseInspector {
+    fn new(url: &str) -> DatabaseInspector {
+        let pool = block_on(MySqlPool::new(url)).unwrap();
+
+        DatabaseInspector { pool }
+    }
+
+    fn get_tables(&self) -> Vec<TableList> {
+        let sql = r"
+    select
+        TABLE_NAME as  table_name,
+        TABLE_TYPE as table_type,
+        TABLE_ROWS as table_rows,
+        INDEX_LENGTH as index_length,
+        AUTO_INCREMENT as auto_increment
+    from information_schema.tables
+    where table_schema=?
+        ";
+
+        block_on(sqlx::query_as::<_, TableList>(sql)
+            .bind("akeneo_pim")
+            .fetch_all(&self.pool)
+            ).unwrap()
+    }
+
+}
+
+fn main() {
+    let inspector = DatabaseInspector::new("mysql://root:root@mysql.lxc/akeneo_pim");
+    for table in inspector.get_tables() {
+        println!("{}", table);
+    }
+}
